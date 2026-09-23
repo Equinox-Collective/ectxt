2546bf72957075b498b710d05520adc40df28ae7
ewasion137 <formanban94@gmail.com>
2026-09-23 18:45:11 +0300

    Buffer 3
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- Makefile
+++ Makefile
@@ -2,15 +2,15 @@ ifeq ($(OS),Windows_NT)
     HOST_OS := Windows
     CC := gcc
     TARGET := ectxt.exe
-    CLEAN_CMD = del /Q /F src\*.o src\libc\*.o src\hal\*.o $(TARGET) 2>NUL || exit 0
+    CLEAN_CMD = del /Q /F src\*.o src\libc\*.o src\hal\*.o src\core\*.o $(TARGET) 2>NUL || exit 0
 else
     HOST_OS := $(shell uname -s)
     CC := gcc
     TARGET := ectxt
-    CLEAN_CMD = rm -f src/*.o src/libc/*.o src/hal/*.o $(TARGET)
+    CLEAN_CMD = rm -f src/*.o src/libc/*.o src/hal/*.o src/core/*.o $(TARGET)
 endif
 
-CFLAGS = -std=c99 -Wall -Wextra -O2 -ffreestanding -nostdlib -Isrc/libc -Isrc -Isrc/hal
+CFLAGS = -std=c99 -Wall -Wextra -O2 -ffreestanding -nostdlib -Isrc/libc -Isrc -Isrc/hal -Isrc/core
 
 ifeq ($(HOST_OS),Windows)
     LDFLAGS = -nostdlib -Wl,-e,mainCRTStartup -lkernel32
@@ -23,6 +23,7 @@ endif
 SRCS = src/main.c \
        src/libc/string.c \
        src/libc/stdio.c \
+       src/core/buffer.c \
        $(HAL_SRC)
 
 OBJS = $(SRCS:.c=.o)
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "hal/hal.h"
+#include "core/buffer.h"
 #include "libc/stdio.h"
 #include "libc/string.h"
 
@@ -15,36 +16,66 @@ int ectxt_main(void) {
         return 1;
     }
 
+    GapBuffer gb;
+    if (!gb_init(&gb, 4096)) {
+        hal_shutdown();
+        return 1;
+    }
+
     int cols = 0, rows = 0;
     hal_get_term_size(&cols, &rows);
 
-    hal_write("\x1b[2J\x1b[H", 7);
-
-    char msg[128];
-    snprintf(msg, sizeof(msg), "ECTXT HAL Test | Screen: %dx%d | Press Ctrl+Q to exit.\r\n", cols, rows);
-    hal_write(msg, strlen(msg));
-
     while (1) {
+        hal_write("\x1b[H\x1b[2J", 7);
+
+        size_t len = gb_length(&gb);
+        for (size_t i = 0; i < len; i++) {
+            char c = gb_char_at(&gb, i);
+            if (c == '\n') {
+                hal_write("\r\n", 2);
+            } else {
+                hal_write(&c, 1);
+            }
+        }
+
+        size_t cur_row = 0, cur_col = 0;
+        gb_get_cursor_coords(&gb, &cur_row, &cur_col);
+
+        char status[128];
+        snprintf(status, sizeof(status), "\x1b[%d;1H\x1b[7m[ECTXT] Ln %d, Col %d | Total Bytes: %d | Ctrl+Q to Exit\x1b[0m", 
+                 rows, (int)cur_row + 1, (int)cur_col + 1, (int)len);
+        hal_write(status, strlen(status));
+
+        char cursor_jump[32];
+        snprintf(cursor_jump, sizeof(cursor_jump), "\x1b[%d;%dH", (int)cur_row + 1, (int)cur_col + 1);
+        hal_write(cursor_jump, strlen(cursor_jump));
+
         int key = hal_read_key();
-        if (key == KEY_NONE) {
-            continue;
-        }
+        if (key == KEY_NONE) continue;
+        if (key == KEY_CTRL_Q) break;
 
-        if (key == KEY_CTRL_Q) {
-            break;
+        switch (key) {
+            case KEY_ARROW_LEFT:  gb_move_left(&gb); break;
+            case KEY_ARROW_RIGHT: gb_move_right(&gb); break;
+            case KEY_ARROW_UP:    gb_move_up(&gb); break;
+            case KEY_ARROW_DOWN:  gb_move_down(&gb); break;
+            case KEY_BACKSPACE:   gb_delete_back(&gb); break;
+            case KEY_DELETE:      gb_delete_forward(&gb); break;
+            case KEY_ENTER:       gb_insert_char(&gb, '\n'); break;
+            default:
+                if (key >= 32 && key <= 126) {
+                    gb_insert_char(&gb, (char)key);
+                } else if (key == 9) {
+                    gb_insert_char(&gb, ' ');
+                    gb_insert_char(&gb, ' ');
+                    gb_insert_char(&gb, ' ');
+                    gb_insert_char(&gb, ' ');
+                }
+                break;
         }
-
-        char kbuf[64];
-        if (key >= 1000) {
-            snprintf(kbuf, sizeof(kbuf), "Special Key Pressed: %d\r\n", key);
-        } else if (key >= 32 && key <= 126) {
-            snprintf(kbuf, sizeof(kbuf), "Char Pressed: '%c' (ASCII %d)\r\n", (char)key, key);
-        } else {
-            snprintf(kbuf, sizeof(kbuf), "Control Code: %d\r\n", key);
-        }
-        hal_write(kbuf, strlen(kbuf));
     }
 
+    gb_free(&gb);
     hal_write("\x1b[2J\x1b[H", 7);
     hal_shutdown();
     return 0;
