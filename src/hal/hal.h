2002784e0e0efb269f5fe6048633a0474b0381e5
ewasion137 <formanban94@gmail.com>
2026-09-23 18:26:43 +0300

    HAL for subsystems
    Signed-off-by: ewasion137 <formanban94@gmail.com>

---
+++ src/hal/hal.h
@@ -0,0 +1,36 @@
+#ifndef HAL_H
+#define HAL_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+enum Key {
+    KEY_NONE = 0,
+    KEY_CTRL_C = 3,
+    KEY_CTRL_Q = 17,
+    KEY_CTRL_S = 19,
+    KEY_ENTER = 13,
+    KEY_ESCAPE = 27,
+    KEY_BACKSPACE = 127,
+    KEY_ARROW_UP = 1000,
+    KEY_ARROW_DOWN,
+    KEY_ARROW_LEFT,
+    KEY_ARROW_RIGHT,
+    KEY_PAGE_UP,
+    KEY_PAGE_DOWN,
+    KEY_HOME,
+    KEY_END,
+    KEY_DELETE
+};
+
+int hal_init(void);
+void hal_shutdown(void);
+void hal_get_term_size(int *cols, int *rows);
+
+int hal_read_key(void);
+void hal_write(const void *buf, size_t len);
+
+void *hal_alloc(size_t size);
+void hal_free(void *ptr, size_t size);
+
+#endif
