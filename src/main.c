78946114947c766cb257777f7cc95ddec3d26059
ewasion137 <formanban94@gmail.com>
2026-09-24 09:53:24 +0300

    Render 3
    Signed-off-by: ewasion137 <formanban94@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "hal/hal.h"
 #include "core/buffer.h"
-#include "libc/stdio.h"
+#include "render/render.h"
 #include "libc/string.h"
 
 __declspec(dllimport) void __stdcall ExitProcess(uint32_t uExitCode);
@@ -22,61 +22,66 @@ int ectxt_main(void) {
         return 1;
     }
 
+    RenderContext render;
+    if (!render_init(&render)) {
+        gb_free(&gb);
+        hal_shutdown();
+        return 1;
+    }
+
     int cols = 0, rows = 0;
     hal_get_term_size(&cols, &rows);
+    render_resize(&render, cols, rows);
 
-    while (1) {
-        hal_write("\x1b[H\x1b[2J", 7);
+    int running = 1;
+    while (running) {
+        hal_get_term_size(&cols, &rows);
+        render_resize(&render, cols, rows);
 
-        size_t len = gb_length(&gb);
-        for (size_t i = 0; i < len; i++) {
-            char c = gb_char_at(&gb, i);
-            if (c == '\n') {
-                hal_write("\r\n", 2);
-            } else {
-                hal_write(&c, 1);
-            }
-        }
-
-        size_t cur_row = 0, cur_col = 0;
-        gb_get_cursor_coords(&gb, &cur_row, &cur_col);
-
-        char status[128];
-        snprintf(status, sizeof(status), "\x1b[%d;1H\x1b[7m[ECTXT] Ln %d, Col %d | Total Bytes: %d | Ctrl+Q to Exit\x1b[0m", 
-                 rows, (int)cur_row + 1, (int)cur_col + 1, (int)len);
-        hal_write(status, strlen(status));
-
-        char cursor_jump[32];
-        snprintf(cursor_jump, sizeof(cursor_jump), "\x1b[%d;%dH", (int)cur_row + 1, (int)cur_col + 1);
-        hal_write(cursor_jump, strlen(cursor_jump));
+        render_update(&render, &gb);
 
         int key = hal_read_key();
         if (key == KEY_NONE) continue;
-        if (key == KEY_CTRL_Q) break;
 
         switch (key) {
-            case KEY_ARROW_LEFT:  gb_move_left(&gb); break;
-            case KEY_ARROW_RIGHT: gb_move_right(&gb); break;
-            case KEY_ARROW_UP:    gb_move_up(&gb); break;
-            case KEY_ARROW_DOWN:  gb_move_down(&gb); break;
-            case KEY_BACKSPACE:   gb_delete_back(&gb); break;
-            case KEY_DELETE:      gb_delete_forward(&gb); break;
-            case KEY_ENTER:       gb_insert_char(&gb, '\n'); break;
+            case KEY_CTRL_Q:
+                running = 0;
+                break;
+            case KEY_ARROW_LEFT:
+                gb_move_left(&gb);
+                break;
+            case KEY_ARROW_RIGHT:
+                gb_move_right(&gb);
+                break;
+            case KEY_ARROW_UP:
+                gb_move_up(&gb);
+                break;
+            case KEY_ARROW_DOWN:
+                gb_move_down(&gb);
+                break;
+            case KEY_BACKSPACE:
+                gb_delete_back(&gb);
+                break;
+            case KEY_DELETE:
+                gb_delete_forward(&gb);
+                break;
+            case KEY_ENTER:
+                gb_insert_char(&gb, '\n');
+                break;
             default:
                 if (key >= 32 && key <= 126) {
                     gb_insert_char(&gb, (char)key);
                 } else if (key == 9) {
-                    gb_insert_char(&gb, ' ');
-                    gb_insert_char(&gb, ' ');
-                    gb_insert_char(&gb, ' ');
-                    gb_insert_char(&gb, ' ');
+                    for (int i = 0; i < 4; i++) {
+                        gb_insert_char(&gb, ' ');
+                    }
                 }
                 break;
         }
     }
 
+    render_free(&render);
     gb_free(&gb);
-    hal_write("\x1b[2J\x1b[H", 7);
     hal_shutdown();
     return 0;
 }
